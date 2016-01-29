af18dd143ac7775131f189b0f8cb878c31cc0a6c
GD1207 <1023251767@qq.com>
2016-01-29 18:32:36 +0800

    Initial Commit

---
+++ GDScrollBanner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GDScrollBanner
+//
+//  Created by xiaoyu on 16/1/29.
+//  Copyright © 2016年 Guoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
